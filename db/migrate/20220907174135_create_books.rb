db0ed3a64dfc1fdce5621c20805c1775193f9139
“yousuf.tabba” <yousuf.tabba@tamu.edu>
2022-09-15 22:49:32 -0500

    hello

--- db/migrate/20220907174135_create_books.rb
+++ db/migrate/20220907174135_create_books.rb
@@ -2,13 +2,9 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
-<<<<<<< HEAD
-=======
       t.string :author
       t.integer :price
       t.date :publishedDate
->>>>>>> 87b43ad ( Checking Everything)
-
       t.timestamps
     end
   end
